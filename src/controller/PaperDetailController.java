d8f4819e9174f2aea19ac934f4bc9ebcd2314c7e
Çağdaş Olam <44732969+cagdasolam@users.noreply.github.com>
2023-05-20 22:53:07 +0300

    paper already in list allert message

--- src/controller/PaperDetailController.java
+++ src/controller/PaperDetailController.java
@@ -1,64 +1,67 @@
 package controller;
 
-import view.PaperDetailFrame;
 import model.Paper;
 import model.ReadingList;
 import model.Researcher;
+import view.PaperDetailFrame;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
 public class PaperDetailController {
-    private PaperDetailFrame view;
-    private Paper paper;
-    private ReadingListController readingListController;
-    private Researcher researcher;
-    
+	private PaperDetailFrame view;
+	private Paper paper;
+	private ReadingListController readingListController;
+	private Researcher researcher;
 
-    public PaperDetailController(Paper paper, Researcher researcher) {
-        view = new PaperDetailFrame();
-        this.paper = paper;
-        this.readingListController = new ReadingListController();
-        this.researcher = researcher;
-        showPaperDetail();
-    }
 
-    public void showPaperDetail() {
-        view.displayPaperDetails(paper);
+	public PaperDetailController(Paper paper, Researcher researcher) {
+		view = new PaperDetailFrame();
+		this.paper = paper;
+		this.readingListController = new ReadingListController();
+		this.researcher = researcher;
+		showPaperDetail();
+	}
 
-        // Populate the reading list combo box
-        List<ReadingList> readingLists = readingListController.findReadingListByUserName(researcher.getName());
-        for (ReadingList readingList : readingLists) {
-            view.getReadingListComboBox().addItem(readingList);
-        }
+	public void showPaperDetail() {
+		view.displayPaperDetails(paper);
 
-        view.getBtnDownload().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                downloadPaper(paper);
-            }
-        });
+		// Populate the reading list combo box
+		List<ReadingList> readingLists = readingListController.findReadingListByUserName(researcher.getName());
+		for (ReadingList readingList : readingLists) {
+			view.getReadingListComboBox().addItem(readingList);
+		}
 
-        view.getBtnAddToReadingList().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ReadingList selectedReadingList = (ReadingList) view.getReadingListComboBox().getSelectedItem();
-                if (selectedReadingList != null) {
-                    addToReadingList(selectedReadingList, paper);
-                }
-            }
-        });
+		view.getBtnDownload().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				downloadPaper(paper);
+			}
+		});
 
-        view.setVisible(true);
-    }
+		view.getBtnAddToReadingList().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ReadingList selectedReadingList = (ReadingList) view.getReadingListComboBox().getSelectedItem();
+				if (selectedReadingList != null) {
+					if (!addToReadingList(selectedReadingList, paper)){
+                        JOptionPane.showMessageDialog(view, "Paper already in list");
+                    }
+				}
+			}
+		});
 
-    private void downloadPaper(Paper paper) {
-        new PaperController().downloadPaper(paper);
-    }
+		view.setVisible(true);
+	}
 
-    private void addToReadingList(ReadingList readingList, Paper paper) {
-    	readingListController.addPaperToReadingList(readingList.getReadingListName(), paper.getTitle(), researcher);
-    }
+	private void downloadPaper(Paper paper) {
+		new PaperController().downloadPaper(paper);
+	}
+
+	private boolean addToReadingList(ReadingList readingList, Paper paper) {
+		return readingListController.addPaperToReadingList(readingList.getReadingListName(), paper.getTitle(), researcher);
+	}
 }
 
